dd5be3fa8b54afe96799fc0d45207ed7486bd694
Ming Yue <35506808+ming1024yue@users.noreply.github.com>
2026-09-05 09:49:13 +0800

    Move subject selection into top navigation

--- src/KnowledgeGraphPlacement.tsx
+++ src/KnowledgeGraphPlacement.tsx
@@ -4,8 +4,8 @@ import KnowledgeGraph from "./KnowledgeGraph";
 export default function KnowledgeGraphPlacement(){
   const container=useRef<HTMLDivElement>(null);
   useLayoutEffect(()=>{
-    const subjects=document.getElementById("subjects");
-    if(subjects&&container.current) subjects.before(container.current);
+    const about=document.getElementById("about");
+    if(about&&container.current) about.before(container.current);
   },[]);
   return <div className="knowledge-placement" ref={container}><KnowledgeGraph/></div>;
 }
